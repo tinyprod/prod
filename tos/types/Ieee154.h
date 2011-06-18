c77d788af627419b08782707fdc24ca81401004d
Eric B. Decker <cire831@gmail.com>
2011-06-18 04:49:52 -0700

    bring in more 802.15.4 header structs from OSIAN

--- tos/types/Ieee154.h
+++ tos/types/Ieee154.h
@@ -1,28 +1,40 @@
 /*
- * "Copyright (c) 2008, 2009 The Regents of the University  of California.
- * All rights reserved."
+ * Copyright (c) 2011 Eric B. Decker
+ * Copyright (c) 2008-2010 The Regents of the University  of California.
+ * All rights reserved.
  *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose, without fee, and without written agreement is
- * hereby granted, provided that the above copyright notice, the following
- * two paragraphs and the author appear in all copies of this software.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
- * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
- * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
- * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
  *
- * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
- * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
- * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
  *
- */
- /*
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @author Stephen Dawson-Haggerty <stevedh@eecs.berkeley.edu>
- * @version $Revision: 1.1 $ $Date: 2009/08/19 17:54:35 $
+ * @author Peter A. Bigot <pab@peoplepowerco.com>
+ * @author Eric B. Decker <cire831@gmail.com>
  */
 
 #ifndef __IEEE154_H__
@@ -43,17 +55,39 @@ typedef struct {
     ieee154_laddr_t laddr;
   } ieee_addr;
 } ieee154_addr_t;
+
 #define i_saddr ieee_addr.saddr
 #define i_laddr ieee_addr.laddr
 
+typedef nx_struct ieee154_header_t {
+  nxle_uint16_t fcf;
+  nxle_uint8_t  dsn;
+  nxle_uint16_t destpan;
+  nxle_uint16_t dest;
+  nxle_uint16_t src;
+} ieee154_header_t;
+
+typedef nx_struct ieee154_fcf_t {
+  nxle_uint16_t frame_type: 3;
+  nxle_uint16_t security_enabled: 1;
+  nxle_uint16_t frame_pending: 1;
+  nxle_uint16_t ack_request: 1;
+  nxle_uint16_t pan_id_compression: 1;
+  nxle_uint16_t _reserved: 3;
+  nxle_uint16_t dest_addr_mode: 2;
+  nxle_uint16_t frame_version: 2;
+  nxle_uint16_t src_addr_mode: 2;
+} ieee154_fcf_t;
+
 enum {
   IEEE154_BROADCAST_ADDR = 0xffff,
-  IEEE154_LINK_MTU   = 127,
+  IEEE154_BROADCAST_PAN  = 0xffff,
+  IEEE154_LINK_MTU       = 127,
 };
 
 struct ieee154_frame_addr {
-  ieee154_addr_t ieee_src;
-  ieee154_addr_t ieee_dst;
+  ieee154_addr_t  ieee_src;
+  ieee154_addr_t  ieee_dst;
   ieee154_panid_t ieee_dstpan;
 };
 
@@ -61,37 +95,57 @@ enum {
   IEEE154_MIN_HDR_SZ = 6,
 };
 
-#if 0
+#ifdef notdef
+
+// deprecated   (does anyone use these?)
+
 struct ieee154_header_base {
   uint8_t length;
   uint16_t fcf;
   uint8_t dsn;
   uint16_t destpan;
 } __attribute__((packed));
-#else
-#endif
+#endif	/* notdef */
 
 enum ieee154_fcf_enums {
-  IEEE154_FCF_FRAME_TYPE = 0,
-  IEEE154_FCF_SECURITY_ENABLED = 3,
-  IEEE154_FCF_FRAME_PENDING = 4,
-  IEEE154_FCF_ACK_REQ = 5,
-  IEEE154_FCF_INTRAPAN = 6,
-  IEEE154_FCF_DEST_ADDR_MODE = 10,
-  IEEE154_FCF_SRC_ADDR_MODE = 14,
+  IEEE154_FCF_FRAME_TYPE	= 0,
+  IEEE154_FCF_SECURITY_ENABLED	= 3,
+  IEEE154_FCF_FRAME_PENDING	= 4,
+  IEEE154_FCF_ACK_REQ		= 5,
+  IEEE154_FCF_INTRAPAN		= 6,
+  IEEE154_FCF_DEST_ADDR_MODE	= 10,
+  IEEE154_FCF_SRC_ADDR_MODE	= 14,
 };
 
 enum ieee154_fcf_type_enums {
-  IEEE154_TYPE_BEACON = 0,
-  IEEE154_TYPE_DATA = 1,
-  IEEE154_TYPE_ACK = 2,
-  IEEE154_TYPE_MAC_CMD = 3,
+  IEEE154_TYPE_BEACON	= 0,
+  IEEE154_TYPE_DATA	= 1,
+  IEEE154_TYPE_ACK	= 2,
+  IEEE154_TYPE_MAC_CMD	= 3,
+  IEEE154_TYPE_MASK	= 7,
 };
 
-enum iee154_fcf_addr_mode_enums {
-  IEEE154_ADDR_NONE = 0,
-  IEEE154_ADDR_SHORT = 2,
-  IEEE154_ADDR_EXT = 3,
+enum ieee154_fcf_addr_mode_enums {
+  IEEE154_ADDR_NONE	= 0,
+  IEEE154_ADDR_SHORT	= 2,
+  IEEE154_ADDR_EXT	= 3,
+  IEEE154_ADDR_MASK	= 3,
 };
 
-#endif
+
+#ifndef DEFINED_TOS_IEEE154_PAN_ID
+// NB: Matches default ActiveMessage group
+#define DEFINED_TOS_IEEE154_PAN_ID 22
+#endif // DEFINED_TOS_IEEE154_PAN_ID
+
+#ifndef DEFINED_TOS_IEEE154_SHORT_ADDRESS
+// NB: Matches default ActiveMessage address
+#define DEFINED_TOS_IEEE154_SHORT_ADDRESS 1
+#endif // DEFINED_TOS_IEEE154_SHORT_ADDRESS
+
+enum {
+  TOS_IEEE154_SHORT_ADDRESS = DEFINED_TOS_IEEE154_SHORT_ADDRESS,
+  TOS_IEEE154_PAN_ID = DEFINED_TOS_IEEE154_PAN_ID,
+};
+
+#endif	/* __IEEE154_H__ */
