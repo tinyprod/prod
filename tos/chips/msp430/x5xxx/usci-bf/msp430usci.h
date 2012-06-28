820f64f51b899d023b9d489be0e6d30cdaee41cf
Eric B. Decker <cire831@gmail.com>
2012-06-28 13:08:23 -0700

    usci-bf: update comments about setting for 1MiHz (should be platform specific)

--- tos/chips/msp430/x5xxx/usci-bf/msp430usci.h
+++ tos/chips/msp430/x5xxx/usci-bf/msp430usci.h
@@ -60,6 +60,16 @@ typedef struct msp430_usci_config_t {
 
 /*
  * The following default configurations assume SMCLK clock is 1MiHz (2^20Hz).
+ *
+ * WARNING: This is not necessarily a good assumption.    Choice of clocks is
+ * really a platform thing and should be provided in some fashion by the platform.
+ *
+ * The common definition here is a carry over from the old initial way that T2 dealt
+ * with this issue.
+ *
+ * In addition, there are issues with assuming binary vs. decimal clocks.  For example
+ * the msp430f5438a with out tweak the PMM wants to be clocked no faster than 8,000,000
+ * (8 MHz).
  */
 
 msp430_usci_config_t msp430_usci_uart_default_config = {
@@ -105,7 +115,7 @@ msp430_usci_config_t msp430_usci_spi_default_config = {
   /* Inactive high MSB-first 8-bit 3-pin master driven by SMCLK */
   ctl0 : UCCKPL | UCMSB | UCMST | UCSYNC,
   ctl1 : UCSSEL__SMCLK,
-  br0  : 2,			/* 2x Prescale */
+  br0  : 2,			/* 2x Prescale, 1*2^19 (512 KiHz) */
   br1  : 0,
   mctl : 0,
   i2coa: 0
@@ -114,13 +124,19 @@ msp430_usci_config_t msp430_usci_spi_default_config = {
 // Should the default be the following?  MM (multi-master) seems
 // like it should be a platform specific thing.
 // ctl0 : (UCMST | UCMODE_3 | UCSYNC),
+//
+// clock divisor is definitely a platform specific thing.   The
+// default should probably be SMCLK/div ~= 100kbps.
+
 
 msp430_usci_config_t msp430_usci_i2c_default_config = {
-  /* 7 bit addressing single I2C master driven by SMCLK */
+  /* 7 bit addressing, multi-master, driven by SMCLK
+   * note: UCMST must be set each time
+   */
   ctl0 : UCSYNC | UCMODE_3 | UCMM,
   ctl1 : UCSSEL__SMCLK,
-  br0  : 10,		/* 104857 hz, slow for slow devices. */
-  br1  : 0,
+  br0  : 10,		/* SMCLK/div */
+  br1  : 0,		/* 1*2^20/div -> 104,858 Hz */
   mctl : 0,
   i2coa: 0x41,
 };
